df201285faec43ca315a0cce8f4d49e752470058
Mirek Sobota <miroslaw.sobota@gmail.com>
2022-12-18 21:42:12 +0100

    Refactoring code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
-let theme = document.querySelector(".js-lightTheme");
-let themeName = document.querySelector(".js-themeName");
+{
+    const welcome = () => {
+        console.log("Welcome on my first webside");
+    }
 
-button.addEventListener("click", () => {
-    body.classList.toggle("js-lightTheme");
+    const onChangeBackGroundClick = () => {
+        body.classList.toggle("js-lightTheme");
+        const body = document.querySelector(".js-body");
+        const themeName = document.querySelector(".js-themeName");
 
-    themeName.innerText = body.classList.contains("js-lightTheme") ? "ciemny" : "jasny";
+        themeName.innerText = body.classList.contains("js-lightTheme") ? "ciemny" : "jasny";
 
-})
+    }
+
+    const init = () => {
+        button.addEventListener("click", onChangeBackGroundClick);
+        const button = document.querySelector(".js-button");
+        welcome();
+    }
+
+    init();
+}
