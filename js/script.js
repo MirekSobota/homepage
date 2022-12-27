4e5e8cb214ca724626d5a7269382445c7e247531
Mirek Sobota <miroslaw.sobota@gmail.com>
2022-12-27 12:13:01 +0100

    refactoring JS, error button and body

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@
     }
 
     const onChangeBackGroundClick = () => {
-        body.classList.toggle("js-lightTheme");
         const body = document.querySelector(".js-body");
         const themeName = document.querySelector(".js-themeName");
-
+        body.classList.toggle("js-lightTheme");
         themeName.innerText = body.classList.contains("js-lightTheme") ? "ciemny" : "jasny";
     }
 
     const init = () => {
-        button.addEventListener("click", onChangeBackGroundClick);
         const button = document.querySelector(".js-button");
+        button.addEventListener("click", onChangeBackGroundClick);
+        
         welcome();
     }
 
